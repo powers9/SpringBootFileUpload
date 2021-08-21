8d1d0c85c077fd7d7800298860a272fa29baa0ff
User <user.push@hackerrank.workspace>
2021-08-21 17:04:01 +0000

    Auto save changes
    Commit by HackerRank

--- src/main/java/com/hackerrank/files/RequestController.java
+++ src/main/java/com/hackerrank/files/RequestController.java
@@ -1,5 +1,14 @@
 package com.hackerrank.files;
 
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,14 +19,31 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class RequestController {
     public static final String UPLOAD_DIR = "uploads/";
+    private final Path fileRoot=Paths.get(UPLOAD_DIR);
 
     @PostMapping("/uploader")
     public ResponseEntity uploader(@RequestParam("fileName") String fileName, @RequestParam("file") MultipartFile file) {
+    try{ 
+      Path fileTobeSaved=fileRoot.resolve(fileName);
+      Files.copy(file.getInputStream(), this.fileRoot.resolve(file.getOriginalFilename()));
+      return new ResponseEntity<>(HttpStatus.CREATED);
+    }catch(Exception e){
+System.out.println(e.getLocalizedMessage());
+    }
         return null;
     }
 
     @GetMapping("/downloader")
     public ResponseEntity downloader(@RequestParam String fileName) {
-        return null;
-    }
+        try{ 
+      Path fileTobeRead=fileRoot.resolve(fileName);
+      Resource resource=new UrlResource(fileTobeRead.toUri());
+     if(resource.exists() || resource.isReadable()){
+return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+resource.getFilename()+"\"").body(resource);
+     } 
+    }catch(Exception e){
+System.out.println(e.getLocalizedMessage());
+     }
+     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+}
 }
